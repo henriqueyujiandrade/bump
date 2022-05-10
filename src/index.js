675ca0df8c6e82686d20c772b80b0701527016c5
henriqueyujiandrade <henrique_yujiandrade@hotmail.com>
2022-05-10 12:33:16 -0300

    feat: ChakraProvider add

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>
   </BrowserRouter>
 );
