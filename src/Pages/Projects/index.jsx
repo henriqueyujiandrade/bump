f87c645e4a3e20a4eec7c4b6481a2799326d708f
maykonx22 <maykon.x23@gmail.com>
2022-05-13 20:01:13 -0400

    feat: Creation of Member and Add Member Modals

--- src/Pages/Projects/index.jsx
+++ src/Pages/Projects/index.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import { ModalMembro } from "../../Modals/ModalMembros";
+import { ModalMembroAdd } from "../../Modals/ModalMembrosAdd";
+
 const Projects = () => {
+    const [openM, setOpenM] = useState(false);
+    const [openMAdd, setOpenMAdd] = useState(false);
+
+    const addMembros = () => {
+        setOpenMAdd(true);
+        setOpenM(false);
+    };
 
     return (
         <>
-        <h2>Projects</h2>
-        
-        </>
-    )
-}
+            {openMAdd && (
+                <ModalMembroAdd setOpenM={setOpenM} setOpenMAdd={setOpenMAdd} />
+            )}
+            {openM && (
+                <ModalMembro addMembros={addMembros} setOpenM={setOpenM} />
+            )}
 
-export default Projects
+            <h2>Projects</h2>
+        </>
+    );
+};
+
+export default Projects;
---
+++ src/components/Pesquisar/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ConteSearching = styled.div`
+    display: flex;
+    flex-direction: column;
+    input {
+        width: 374px;
+        padding: 10px 20px;
+        border-radius: 15px;
+        font-family: "Inter";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 36px;
+
+        color: #acabab;
+    }
+`;
+
+export const DivsaoSearching = styled.div`
+    width: 100%;
+    height: 2px;
+    background-color: black;
+    margin: 25px 0;
+`;
