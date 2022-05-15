324d19f031e30f0cc741f721a506b3e3c67bfed8
Igor Xavier <95296668+igor-xavier@users.noreply.github.com>
2022-05-15 16:06:05 -0300

    Revert "Feature dashboard/task"

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,132 +1,59 @@
 import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Stack,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
-  Link,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-
-import * as yup from 'yup';
-import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-
-import Header from "../../components/Header"
-
-const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Link,
+    Stack,
+    Image,
+  } from '@chakra-ui/react';
+  
+  const Login = () => {
 
 
-  const schema = yup.object().shape({
-    email: yup.string().required("Campo Obrigatório").email("Email inválido"),
-    password: yup.string().required("Campo Obrigatório").min(6, "Minimo 6 caracteres!"),
-  })
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit = (data) => {
-    console.log(data)
-  }
-
-  return (
-    <>
-    <Header register/>
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
-            Login
-          </Heading>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={8}>
-
-            <form onSubmit={handleSubmit(onSubmit)} spacing={8}>
-              <FormControl  >
-                <FormLabel>Email</FormLabel>
-                <Input type="email" id="email" {...register("email")} />
-                {errors.email && (
-                   <> 
-                   <span className="email">{errors.email.message}</span>
-                   <Stack align={'center'} >
-                    <Heading fontSize={'1x1'} textAlign={'center'}>
-                    {errors.email.message}
-                    </Heading>
-                    </Stack>
-                    </>
-                )}
-              </FormControl>
-              <FormControl {...register("password")}>
-                <FormLabel>Senha</FormLabel>
-                <InputGroup>
-                  <Input id="password" {...register("password")} type={showPassword ? 'text' : 'password'} />
-                  <InputRightElement h={'full'}>
-                    <Button
-                      variant={'ghost'}
-                      onClick={() =>
-                        setShowPassword((showPassword) => !showPassword)
-                      }>
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                {errors.password && (
-                   <> 
-                   <span className="error">{errors.password.message}</span>
-                   <Stack align={'center'} spacing={6}>
-                    <Heading fontSize={'1x1'} textAlign={'center'}>
-                    {errors.password.message}
-                    </Heading>
-                    </Stack>
-                    </>
-                )}
-              </FormControl>
-              <Stack spacing={10} pt={2}>
-                <Button
-                  type="submit"
-                  loadingText="Submitting"
-                  size="lg"
-                  bg={'#8518F5'}
-                  color={'white'}
-                  _hover={{
-                    bg: '#AF3DFF',
-                  }}>
-                  Login
-                </Button>
+    
+    return (
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack spacing={4} w={'full'} maxW={'md'}>
+            <Heading fontSize={'2xl'}>Sign in to your account</Heading>
+            <FormControl  id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.500'}>Forgot password?</Link>
               </Stack>
-            </form>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Não tem conta ainda? <Link to="/" color={'#8518F5'}>Login</Link>
-              </Text>
+              <Button colorScheme={'blue'} variant={'solid'}>
+                Sign in
+              </Button>
             </Stack>
           </Stack>
-        </Box>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={'Login Image'}
+            objectFit={'cover'}
+            src={
+              'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+            }
+          />
+        </Flex>
       </Stack>
-    </Flex>
-    </>
-  );
-}
+    );
+  }
 
-export default Login
-
-
+  export default Login
--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'https://bump-capstone.herokuapp.com/'
+    baseURL:'https:/.herokuapp.com/'
 })
 
 export default api
