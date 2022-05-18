06f0697ca1907613519d5d7a209fb29bd30d7a82
joaovictorprocopio <victor.345@hotmail.com>
2022-05-18 02:33:08 -0300

    feat: finished styling and responsiveness

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,60 +1,169 @@
-import { Flex, Heading, Button, Input } from "@chakra-ui/react";
+import {
+    Flex,
+    Box,
+    Heading,
+    Button,
+    Input,
+    CloseButton,
+} from "@chakra-ui/react";
 
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ChatContext } from "../../Providers/Chat/index.js";
+import { useParams } from "react-router-dom";
 
 const Chat = () => {
+    // const params = useParams();
+    // const userInfo = JSON.parse(localStorage.getItem("@bump:myInfo"));
+    // const { chat, addChat, removeChat } = useContext(ChatContext);
     const [msg, setMsg] = useState("");
 
-    const mensagemExemplo = [
-        {
-            description: "sei la to testando email 1",
-            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
-            userId: 2,
-            id: 1,
-        },
-    ];
     const mensagensTeste = [
         {
+            name: "Joao",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989851c5badb",
             userId: 2,
             id: 1,
         },
         {
+            name: "Yasmin",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
             userId: 3,
-            id: 1,
+            id: 2,
         },
         {
+            name: "Henrique",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
             userId: 4,
-            id: 1,
+            id: 3,
         },
         {
+            name: "Joao",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
             userId: 2,
-            id: 1,
+            id: 4,
         },
         {
+            name: "Joao",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
             userId: 2,
-            id: 1,
+            id: 5,
         },
         {
+            name: "Joao",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989751c5aadb",
             userId: 2,
-            id: 1,
+            id: 6,
         },
         {
+            name: "Yasmin",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 3,
+            id: 7,
+        },
+        {
+            name: "Yasmin",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 3,
+            id: 2,
+        },
+        {
+            name: "Joao",
             description: "sei la to testando email 1",
             groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
             userId: 2,
-            id: 1,
+            id: 5,
+        },
+        {
+            name: "Yasmin",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 3,
+            id: 2,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Yasmin",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 3,
+            id: 2,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Yasmin",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 3,
+            id: 2,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Yasmin",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 3,
+            id: 2,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
+        },
+        {
+            name: "Joao",
+            description: "sei la to testando email 1",
+            groupId: "8da51ca4-204f-4264-a9bb-989851c5aadb",
+            userId: 2,
+            id: 5,
         },
     ];
     const userId = 2;
@@ -73,58 +182,115 @@ const Chat = () => {
     return (
         <>
             <Flex
+                position={["absolute", "absolute", "static"]}
+                top={"0px"}
+                left={"0px"}
                 flexDir={"column"}
-                w={"386px"}
-                h={"718px"}
-                borderRadius={"61px"}
+                w={["100vw", "100vw", "308.8px"]}
+                h={["100vh", "100vh", "514.4px"]}
+                borderRadius={["0px", "0px", "48.8px"]}
                 backgroundColor={"#ffffff"}
-                p={"0px 0px 36px 0px"}
+                p={"0px 0px 28.8px 0px"}
                 justifyContent={"space-between"}
             >
-                <Heading
-                    borderRadius={"61px 61px 0px 0px"}
-                    w={"386px"}
-                    h={"73px"}
+                <Flex
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                    p={"12.8px"}
+                    borderRadius={["0px", "0px", "48.8px 48.8px 0px 0px"]}
+                    w={["100vw", "100vw", "308.8px"]}
+                    h={"58.4px"}
                     backgroundColor={"#6D17B0"}
-                    color={"#fff"}
                 >
-                    Chat
-                </Heading>
-                <Flex flexDir={"column"} overflowY={"auto"}>
-                    {groupMsg?.map((item) => {
-                        return (
-                            <Flex
-                                alignSelf={
-                                    userId === item.userId
-                                        ? "flex-end"
-                                        : "flex-start"
-                                }
-                                backgroundColor={
-                                    userId === item.userId
-                                        ? "rgba(195, 118, 255, 0.98)"
-                                        : "#fff"
-                                }
-                                borderRadius={"5x"}
-                            >
-                                {item.description}
-                            </Flex>
-                        );
-                    })}
+                    <Heading color={"#fff"}>Chat</Heading>
+                    <CloseButton color={"#fff"} size="lg" />
                 </Flex>
-                <Flex>
+
+                <Box
+                    transform={"rotate(180deg)"}
+                    scroll
+                    overflowY="auto"
+                    w={["100vw", "100vw", "308.8px"]}
+                    h={["82vh", "82vh", "432px"]}
+                    css={{
+                        "&::-webkit-scrollbar": {
+                            width: "4px",
+                        },
+                        "&::-webkit-scrollbar-track": {
+                            width: "6px",
+                        },
+                        "&::-webkit-scrollbar-thumb": {
+                            background: "rgba(195, 118, 255, 0.98)",
+                            borderRadius: "24px",
+                        },
+                        "&": {
+                            direction: "rtl",
+                        },
+                    }}
+                >
+                    <Flex
+                        transform={"rotate(180deg)"}
+                        p={"12px"}
+                        flexDir={"column"}
+                        justifyContent={"flex-end"}
+                        fontSize={["24px", "22px", "16px"]}
+                    >
+                        {groupMsg?.map((item) => {
+                            return (
+                                <Flex
+                                    css={{
+                                        "&": {
+                                            direction: "ltr",
+                                        },
+                                    }}
+                                    flexDir={"column-reverse"}
+                                    w={["67vw", "50vw", "160px"]}
+                                    alignSelf={
+                                        userId === item.userId
+                                            ? "flex-start"
+                                            : "flex-end"
+                                    }
+                                    backgroundColor={
+                                        userId === item.userId
+                                            ? "rgba(195, 118, 255, 0.98)"
+                                            : "#c9c6c6"
+                                    }
+                                    borderRadius={"10px"}
+                                    margin={"5px 0px 5px 0px"}
+                                    p={"5px 10px 5px 10px"}
+                                    _after={
+                                        userId === item.userId
+                                            ? {}
+                                            : {
+                                                  content: `'${item.name}:'`,
+                                                  color: "#430d6d",
+                                              }
+                                    }
+                                >
+                                    {item.description}
+                                </Flex>
+                            );
+                        })}
+                    </Flex>
+                </Box>
+
+                <Flex alignItems={"center"} justifyContent={"space-around"}>
                     <Input
-                        w={"327px"}
-                        h={"55px"}
-                        borderRadius={"46px"}
+                        w={["75vw", "75vw", "224px"]}
+                        h={["59px", "59px", "44px"]}
+                        borderRadius={"36.8px"}
                         backgroundColor={"#BDBDBD"}
                         value={msg}
                         onChange={(e) => {
                             setMsg(e.target.value);
                         }}
-                        color={"#333"}
+                        color={"#000"}
                         placeholder={"Digite sua mensagem"}
+                        _placeholder={{ color: "#333" }}
+                        fontSize={["20px", "20px", "16px"]}
                     />
                     <Button
+                        fontSize={["20px", "20px", "16px"]}
                         color={"#fff"}
                         backgroundColor={"#6D17B0"}
                         onClick={handleSubmit}
