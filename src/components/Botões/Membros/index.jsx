617576c81846fb1c14273555172f77db6fbf58af
maykonx22 <maykon.x23@gmail.com>
2022-05-19 01:42:00 -0400

    feat: Modal Creation Delete

--- src/components/Botões/Membros/index.jsx
+++ src/components/Botões/Membros/index.jsx
@@ -1,14 +1,48 @@
-import { BtnMembros } from "../style";
+import { BtnMembros, BtnMembrosC, BtnMembrosE } from "../style";
 
-export const BtnMembrosAdd = ({ children, click, type }) => {
+export const BtnMembrosAdd = ({
+    children,
+    click,
+    type,
+    excluir,
+    cancelar,
+    modal,
+}) => {
     return (
         <>
-            {click ? (
-                <BtnMembros type={type} onClick={click}>
-                    {children}
-                </BtnMembros>
-            ) : (
-                <BtnMembros type={type}>{children}</BtnMembros>
+            {excluir && (
+                <>
+                    {click ? (
+                        <BtnMembrosE type={type} onClick={click}>
+                            {children}
+                        </BtnMembrosE>
+                    ) : (
+                        <BtnMembrosE type={type}>{children}</BtnMembrosE>
+                    )}
+                </>
+            )}
+
+            {cancelar && (
+                <>
+                    {click ? (
+                        <BtnMembrosC type={type} onClick={click}>
+                            {children}
+                        </BtnMembrosC>
+                    ) : (
+                        <BtnMembrosC type={type}>{children}</BtnMembrosC>
+                    )}
+                </>
+            )}
+            {modal && (
+                <>
+                    {click ? (
+                        <BtnMembros type={type} onClick={click}>
+                            {children}
+                        </BtnMembros>
+                    ) : (
+                        <BtnMembros type={type}>{children}</BtnMembros>
+                    )}
+                </>
             )}
         </>
     );
